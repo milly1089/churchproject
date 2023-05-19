4af40dcffcaf9269c3e22f856350274819c8ab62
kilifice <118257325+milly1089@users.noreply.github.com>
2023-05-19 11:51:10 +0300

    kilifi central
    added facebook link

--- resources/views/blog-single.blade.php
+++ resources/views/blog-single.blade.php
@@ -78,7 +78,7 @@
 {{--                <h4>Jane Smith</h4>--}}
 {{--                <div class="social-links">--}}
 {{--                  <a href="https://twitters.com/#"><i class="bi bi-twitter"></i></a>--}}
-{{--                  <a href="https://facebook.com/#"><i class="bi bi-facebook"></i></a>--}}
+{{--                  <a href="https://facebook.com/kilifi central"><i class="bi bi-facebook"></i></a>--}}
 {{--                  <a href="https://instagram.com/#"><i class="biu bi-instagram"></i></a>--}}
 {{--                </div>--}}
 {{--                <p>--}}
